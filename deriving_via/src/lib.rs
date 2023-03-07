66372099286448a44b9e1ef539c0d4b7502f2f70
mitama <mitama@mitama.tech>
2023-03-08 01:49:20 +0900

    feat(docs): module doc

--- deriving_via/src/lib.rs
+++ deriving_via/src/lib.rs
@@ -1,3 +1,47 @@
+//! # deriving_via
+//!
+//! This library provides the `DerivingVia` derive macro for _Newtypes_ (Single-Field structs).
+//! `DerivingVia` can be used like [_derive_more_](https://docs.rs/derive_more/latest/derive_more/).
+//!
+//! ## Generalised Newtype Deriving
+//!
+//! ### Example
+//!
+//! ```
+//! use deriving_via::DerivingVia;
+//!
+//! #[derive(DerivingVia)]
+//! #[deriving(Display)]
+//! struct Newtype(pub i32);
+//!
+//! let x = Newtype(0);
+//!
+//! println!("{x}");
+//! ```
+//!
+//! ## Deriving Via
+//!
+//! Deriving Via allows deriving from beyond a multiply wrapped hierarchy
+//! using transitive type conversion through `Deref` traits.
+//!
+//! ### Example
+//!
+//! ```
+//! use deriving_via::DerivingVia;
+//!
+//! #[derive(DerivingVia)]
+//! pub struct Inner(i32);
+//!
+//! #[derive(DerivingVia)]
+//! #[deriving(Display(via = i32))]
+//! pub struct Outer(Inner);
+//!
+//! let x = Outer(Inner(42));
+//!
+//! println!("{x}");
+//! ```
+//!
+
 mod impls;
 mod utils;
 
