0ccec6d2b130f8ca05e29bbed2a368bb53ac3b86
mitama <mitama@mitama.tech>
2023-03-11 19:50:46 +0900

    chore(deriving_via): remove unused functions

--- deriving_via/src/utils.rs
+++ deriving_via/src/utils.rs
@@ -2,38 +2,6 @@ use proc_macro2::TokenStream;
 use proc_macro_error::abort;
 use quote::quote;
 
-#[allow(unused)]
-pub(crate) fn combine<const N: usize>(
-    impls: [fn(&syn::DeriveInput) -> proc_macro2::TokenStream; N],
-) -> impl FnOnce(&syn::DeriveInput) -> proc_macro::TokenStream {
-    move |ast: &syn::DeriveInput| -> proc_macro::TokenStream {
-        Vec::<proc_macro::TokenStream>::from_iter(impls.map(|derive| derive(ast).into()))
-            .into_iter()
-            .collect()
-    }
-}
-
-#[allow(unused)]
-pub(crate) fn extract_single_field(ast: &syn::DeriveInput) -> syn::Field {
-    match ast.data {
-        syn::Data::Struct(syn::DataStruct { ref fields, .. }) => {
-            match fields.iter().cloned().collect::<Vec<_>>().as_slice() {
-                [field] => field.clone(),
-                _ => abort!(
-                    ast,
-                    "Not just one field";
-                    help = "#[derive(DerivingVia)] can only be used with one field";
-                ),
-            }
-        }
-        _ => abort!(
-            ast,
-            "input is not a struct";
-            help = "#[derive(DerivingVia)] can only be used with structs";
-        ),
-    }
-}
-
 pub(crate) type Constructor = TokenStream;
 pub(crate) type Accessor = TokenStream;
 pub(crate) type UnderlyingType = syn::Type;
